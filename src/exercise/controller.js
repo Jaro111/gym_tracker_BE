96e4b8a1e8988720f68b4f6976de32ccc875ffff
Jaro111 <jarokark@protonmail.com>
2025-03-21 16:08:13 +0000

    feat(db): add weight and some other data to modle

--- src/exercise/controller.js
+++ src/exercise/controller.js
@@ -1,1 +1,17 @@
 const Exercise = require("./model");
+
+// Create exercise
+
+const addExercise = async (req, res) => {
+  try {
+    const exercise = await Exercise.create({
+      name: req.body.name,
+    });
+
+    res.status(200).json({ message: "Exercise created", exercise: exercise });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = addExercise;
