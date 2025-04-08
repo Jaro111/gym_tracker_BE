7acd97dea31d58b2a2f7ba5f1dc2503f80e73328
Jaro111 <jarokark@protonmail.com>
2025-04-08 09:28:15 +0100

    chore: Deleted coments

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,15 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(process.env.URI, {
-//   dialect: "postgres",
-//   logging: false, // Set to true for debugging
-// });
-
-// sequelize.authenticate();
-// console.log("DataBase connection is working...");
-
-// backend/test.js
-
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.AWS_URI,
